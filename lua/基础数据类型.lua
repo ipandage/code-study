2346906c6a7ab9c9592102b115525f4a15f0c87c
ipandage <546657868@qq.com>
2016-06-29 13:52:55 +0800

    lua

---
+++ lua/基础数据类型.lua
@@ -0,0 +1,73 @@
+print(type("hello world")) -->output:string
+print(type(print))         -->output:function
+print(type(true))          -->output:boolean
+print(type(360.0))         -->output:number
+print(type(nil))           -->output:nil
+
+-- nil（空）
+local num
+print(num)        -->output:nil
+
+num = 100
+print(num)        -->output:100
+
+-- boolean（布尔）
+-- 布尔类型，可选值 true/false；Lua 中 nil 和 false 为“假”，其它所有值均为“真”。比如 0 和空字符串就是“真”；
+local a = true
+local b = 0
+local c = nil
+if a then
+    print("a")        -->output:a
+else
+    print("not a")    --这个没有执行
+end
+
+if b then
+    print("b")        -->output:b
+else
+    print("not b")    --这个没有执行
+end
+
+if c then
+    print("c")        --这个没有执行
+else
+    print("not c")    -->output:not c
+end
+
+-- number（数字）
+-- Number 类型用于表示实数，和 C/C++ 里面的 double 类型很类似。可以使用数学函数 math.floor（向下取整）和 math.ceil（向上取整）进行取整操作。
+local order = 3.99
+local score = 98.01
+print(math.floor(order))   -->output:3
+print(math.ceil(score))    -->output:99
+
+-- string（字符串）
+local str1 = 'hello world'
+local str2 = "hello lua"
+local str3 = [["add\name",'hello']]
+local str4 = [=[string have a [[]].]=]
+
+print(str1)    -->output:hello world
+print(str2)    -->output:hello lua
+print(str3)    -->output:"add\name",'hello'
+print(str4)    -->output:string have a [[]].
+
+-- table (表)
+local corp = {
+    web = "www.google.com",   --索引为字符串，key = "web",
+                              --            value = "www.google.com"
+    telephone = "12345678",   --索引为字符串
+    staff = {"Jack", "Scott", "Gary"}, --索引为字符串，值也是一个表
+    100876,              --相当于 [1] = 100876，此时索引为数字
+                         --      key = 1, value = 100876
+    100191,              --相当于 [2] = 100191，此时索引为数字
+    [10] = 360,          --直接把数字索引给出
+    ["city"] = "Beijing" --索引为字符串
+}
+
+print(corp.web)               -->output:www.google.com
+print(corp["telephone"])      -->output:12345678
+print(corp[2])                -->output:100191
+print(corp["city"])           -->output:"Beijing"
+print(corp.staff[1])          -->output:Jack
+print(corp[10])               -->output:360
---
+++ lua/表达式.lua
@@ -0,0 +1,18 @@
+-- 算术运算符
+print(1 + 2)       -->打印 3
+print(5 / 10)      -->打印 0.5。 这是Lua不同于c语言的
+print(5.0 / 10)    -->打印 0.5。 浮点数相除的结果是浮点数
+-- print(10 / 0)   -->注意除数不能为0，计算的结果会出错
+print(2 ^ 10)      -->打印 1024。 求2的10次方
+
+local num = 1357
+print(num % 2)       -->打印 1
+print((num % 2) == 1) -->打印 true。 判断num是否为奇数
+print((num % 5) == 0)  -->打印 false。判断num是否能被5整数
+
+-- 关系运算符
+print(1 < 2)    -->打印 true
+print(1 == 2)   -->打印 false
+print(1 ~= 2)   -->打印 true
+local a, b = true, false
+print(a == b)  -->打印 false
