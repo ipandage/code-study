5aa7fa7c63cc4c28c7d5031bd8e2f3c4561534e2
gaoxingang <gaoxingang@pinwheelmedical.com>
2018-02-01 13:35:06 +0800

    test

---
+++ basic/src/main/java/jvm/gc/SystemGcTest.java
@@ -0,0 +1,11 @@
+package jvm.gc;
+
+/**
+ * @author gxg
+ * @date 2018/1/23 下午4:16
+ */
+public class SystemGcTest {
+    public static void main(String[] args) {
+        Runtime.getRuntime().gc(); //
+    }
+}
